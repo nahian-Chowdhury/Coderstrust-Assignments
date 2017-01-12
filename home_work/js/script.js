287d3274444190612c73ff3daeccd5766bc3ef36
Md. Al Nahian Chowdhury <nahian.red@gmail.com>
2017-01-12 21:38:42 +0600

    home word added for next class

---
+++ home_work/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	$("button").click(function(){
+		$("#title").text("Hello Linkon");
+	});
+});
+
+
+
+$(document).ready(function(){
+	$("#btn1").click(function(){
+
+		var a = $("#two").text();
+
+		$("#one").text(a);
+
+	});
+
+
+});
